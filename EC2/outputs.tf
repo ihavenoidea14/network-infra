67450ec56881b5a9b7738c7b0adbec9a826c914c
ihavenoidea14 <jarid.wade@icloud.com>
2018-08-04 19:35:31 -0400

    Adding up address to outputs

--- EC2/outputs.tf
+++ EC2/outputs.tf
@@ -1,3 +1,11 @@
 output "instanceId" {
   value = "${aws_instance.instance.id}"
 }
+
+output "public-ip-address" {
+  value = "${aws_instance.instance.public_ip}"
+}
+
+output "private-ip-address" {
+  value = "${aws_instance.instance.private_ip}"
+}
--- Network/VPC-UE1/outputs.tf
+++ Network/VPC-UE1/outputs.tf
@@ -5,3 +5,11 @@ output "lambda-subnet-prod" {
 output "lambda-subnet-dev" {
   value = "${module.lambda-subnet-dev.subnetId}"
 }
+
+output "jump-box-ip" {
+  value = "${module.jump.public-ip-address}"
+}
+
+output "private-box-ip" {
+  value = "${module.jump.private-ip-address}"
+}
